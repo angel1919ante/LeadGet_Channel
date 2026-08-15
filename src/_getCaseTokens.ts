ae5cca679c247a138e0b47efc5ed2b558876e474
Egor <dxerfgcy2o@gmail.com>
2026-08-15 12:47:03 +0300

    chore: remove one-off case-token/content-plan scripts and jobs

--- src/_getCaseTokens.ts
+++
@@ -1,7 +0,0 @@
-import { getCasesRows } from './sheets.ts';
-const rows = await getCasesRows();
-for (const r of rows) {
-  if (r.status === 'approved') {
-    console.log(`${r.client} | token=${r.token} | ${r.sent}/${r.leads}/${r.conversion}%`);
-  }
-}
