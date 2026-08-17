d88d6617d7308a7e7a917b788a9bad9da7a05b06
Egor <dxerfgcy2o@gmail.com>
2026-08-17 15:10:19 +0300

    chore: remove one-off feature-posts listing script/job

--- src/_listFeaturePosts.ts
+++
@@ -1,6 +0,0 @@
-import { getAutoPostRows } from './sheets.ts';
-const rows = await getAutoPostRows();
-const features = rows.filter(r => r.postType === 'фича');
-for (const r of features) {
-  console.log(r.status, r.sourceUrl);
-}
