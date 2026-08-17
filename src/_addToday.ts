4d0184f200dff41fc0057445b18bbd6eea9f2e36
Egor <dxerfgcy2o@gmail.com>
2026-08-17 15:57:12 +0300

    chore: remove one-off add-today script and job

--- src/_addToday.ts
+++
@@ -1,9 +0,0 @@
-import { appendContentPlanRow, getContentPlanRows } from './sheets.ts';
-const today = '17.08.2026';
-const plans = await getContentPlanRows();
-if (plans.some((p) => p.date === today)) {
-  console.log('на сегодня уже есть строка, ничего не делаю');
-} else {
-  await appendContentPlanRow({ date: today, type: 'новость', title: '', token: '', data: '{}' });
-  console.log(`добавлена строка "новость" на ${today}`);
-}
