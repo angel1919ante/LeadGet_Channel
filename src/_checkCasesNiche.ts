f0580108f9a88db8d93d41233c8e10d9ba5a7449
Egor <dxerfgcy2o@gmail.com>
2026-08-17 23:34:09 +0300

    chore: remove one-off set-niche script and job

--- src/_checkCasesNiche.ts
+++
@@ -1,7 +0,0 @@
-import { getCasesRows } from './sheets.ts';
-const rows = await getCasesRows();
-for (const r of rows) {
-  if (r.token === '046b6786-4da0-49f3-a39d-6328b616d8f2' || r.token === '99eb4410-9861-4e49-9bb3-18e78f2acb51') {
-    console.log(r.client, '|', r.niche, '|', r.token);
-  }
-}
