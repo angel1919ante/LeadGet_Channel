1f5fbbca37e768e2d8b5a20929e970f1a381c83d
Egor <dxerfgcy2o@gmail.com>
2026-08-18 12:26:03 +0300

    temp: delete overflowing Ankrait chat album before re-posting fixed version

--- src/_findAnkraitPosts.ts
+++ src/_findAnkraitPosts.ts
@@ -10,10 +10,8 @@ async function main() {
   const client = new TelegramClient(new StringSession(session), apiId, apiHash, { connectionRetries: 3 });
   await client.connect();
 
-  const messages = await client.getMessages(channel, { limit: 8 });
-  for (const m of messages) {
-    console.log(`id=${m.id} groupedId=${m.groupedId ?? ''} media=${!!m.media} date=${m.date}`);
-  }
+  await client.deleteMessages(channel, [58, 59, 60, 61], { revoke: true });
+  console.log('deleted second (overflowing) chat album (ids 58-61)');
 
   await client.disconnect();
 }
