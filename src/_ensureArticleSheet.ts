a22c089a98220a4ae4f285dd489f3e318d5561c7
Egor <dxerfgcy2o@gmail.com>
2026-08-24 20:56:50 +0300

    chore: remove one-off Articles sheet init script and job

--- src/_ensureArticleSheet.ts
+++
@@ -1,8 +0,0 @@
-import { ensureArticleSheet } from './sheets.ts';
-
-ensureArticleSheet()
-  .then(() => console.log('Articles sheet ready'))
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
